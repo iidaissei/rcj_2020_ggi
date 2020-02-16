a4ab416c3af7dce768dafe17f7f9cf358a0d3b4e
iidaissei <issei.288.higashi@icloud.com>
2020-02-16 21:56:09 +0900

    first_commit

---
+++ src/sm_test_phase.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------
+# Title: RCJ2020_GoGetItのTestPhase用プログラム
+# Author: Issei Iida
+# Date: 2020/02/12
+# Memo: オーダーはBringmeなので、現在位置確認処理は省く
+#----------------------------------------------------------
+
+# Python
+import sys
+# ROS
+import rospy
+import smach
+import smach_ros
+
+sys.path.insert(0, '/home/issei/catkin_ws/src/mimi_common_pkg/scripts')
+# from common_action_client import *
+# from common_function import *
+
+
+class ListenCommand(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,
+                             outcomes = ['listen_success',
+                                         'listen_failure',
+                                         'next_cmd',
+                                         'all_cmd_finish'],
+                             output_keys = ['cmd_out'])
+        # ServiceProxy
+        self.listen_srv = rospy.ServiceProxy('/gpsr/actionplan', ActionPlan)
+        # Value
+        self.listen_count = 1
+        self.cmd_count = 1
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state: LISTEN_COMMAND')
+        if self.cmd_count == 4:
+            speak('Finish all command')
+            return 'all_cmd_finish'
+        elif self.listen_count <= 3:
+            speak('CommandNumber is ' + str(self.cmd_count))
+            speak('ListenCount is ' + str(self.listen_count))
+            speak('Please instruct me')
+            result = self.listen_srv()
+            if result.result:
+                self.listen_count = 1
+                self.cmd_count += 1
+                userdata.cmd_out = result
+                return 'listen_success'
+            else:
+                self.listen_count += 1
+                speak("Sorry, I could't listen")
+                return 'listen_failure'
+        else:
+            speak("I couldn't understand the instruction")
+            self.listen_count = 1
+            self.cmd_count +=1
+            return 'next_cmd'
+
+
+class ExeAction(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,
+                             outcomes = ['action_success',
+                                         'action_failure'],
+                             input_keys = ['cmd_in'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state: EXE_ACTION')
+        action = userdata.cmd_in.action
+        data = userdata.cmd_in.data
+        print data
+        print action
+        # result = exeActionPlan(action, data)
+        result = True
+        # if result.result:
+        if result:
+            speak('Action success')
+            return 'action_success'
+        else:
+            speak('Action failed')
+            return 'action_failure'
+
+
+class Exit(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,
+                             outcomes = ['finish_exit'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state: EXIT')
+        # coord_list = searchLocationName('', 'exit')
+        # result = navigationAC(coord_list)
+        result = True
+        if result:
+            return 'finish_exit'
+        else:
+            # speak('I can not mave exit')
+            print 'exit'
+            return 'finish_exit'
+
+
+class GgiTestPhaseServer():
+    def __init__(self):
+        self.server = rospy.Service('/ggi/testphase', GgiTestPhase, self.startSM)
+
+
+    def startSM(self, req):
+        sm_top = smach.StateMachine(outcomes = ['finish_sm_top'])
+        with sm_top:
+            smach.StateMachine.add(
+                    'LISTEN_COMMAND',
+                    ListenCommand(),
+                    transitions = {'listen_success':'EXE_ACTION',
+                                   'listen_failure':'LISTEN_COMMAND',
+                                   'next_cmd':'DECIDE_MOVE',
+                                   'all_cmd_finish':'EXIT'},
+                    remapping = {'cmd_out':'cmd'})
+
+            smach.StateMachine.add(
+                    'EXE_ACTION',
+                    ExeAction(),
+                    transitions = {'action_success':'DECIDE_MOVE',
+                                   'action_failure':'DECIDE_MOVE'},
+                    remapping = {'cmd_in':'cmd'})
+
+            smach.StateMachine.add(
+                    'EXIT',
+                    Exit(),
+                    transitions = {'exit_finish':'finish_sm_top'})
+
+        outcomes = sm_top.execute()
+
+        return GgiTestPhaseResponse(result = True)
+
+
+if __name__ == '__main__':
+    rospy.init_node('sm_test_phase', anonymous = True)
+    m = GgiTestPhaseServer()
+    rospy.spin()
---
+++ src/sm_training_phase.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#---------------------------------------------------------------------
+# Title: RCJ2020_GoGetItのTrainingPhase用プログラム
+# Author: Issei Iida
+# Date: 2020/02/16
+#---------------------------------------------------------------------
+
+# Python
+import sys
+# ROS
+import rospy
+import rosparam
+import smach
+import smach_ros
+from std_msgs.msg import String
+from ggi.srv import ListenCommand, GgiLearning, YesNo
+from rcj_2020_ggi.srv import GgiTestPhase
+
+sys.path.insert(0, '/home/athome/catkin_ws/src/mimi_common_pkg/scripts/')
+from common_function import BaseCarrier, speak
+
+
+class Listen(smach.State): 
+    def __init__(self):
+        smach.State.__init__(
+                self,
+                outcomes = ['listen_failed',
+                            'motion',
+                            'event'],
+                output_keys = ['cmd_output'])
+        # Service
+        self.listen_cmd_srv = rospy.ServiceProxy('/listen_command', ListenCommand)
+        # Value
+        self.cmd_dict = rosparam.get_param('/ggi/voice_cmd')
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state: LISTEN')
+        result = self.listen_cmd_srv(file_name = 'voice_cmd')
+        if result.result:
+            command = result.cmd
+            userdata.cmd_output = command
+            userdata.words_output = self.words_dict[command]
+            state = self.cmd_dict[command]
+            return state
+        else:
+            speak("I could't hear")
+            return 'listen_failed'
+
+
+class Motion(smach.State):
+    def __init__(self):
+        smach.State.__init__(
+                self,
+                outcomes = ['finish_motion'],
+                input_keys = ['cmd_input'])
+        self.bc = BaseCarrier()
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state: MOVE')
+        if userdata.cmd_input == 'turn right':
+            speak('Rotate right')
+            self.bc.angleRotation(-45)
+        elif userdata.cmd_input == 'turn left':
+            speak('Rotate left')
+            self.bc.angleRotation(45)
+        elif userdata.cmd_input == 'go straight':
+            speak('Go forward')
+            self.bc.translateDist(0.20)
+        elif userdata.cmd_input == 'go back':
+            speak('Go back')
+            self.bc.translateDist(-0.20)
+        else:
+            pass
+        return 'finish_motion'
+
+
+class Event(smach.State):
+    def __init__(self):
+        smach.State.__init__(
+                self,
+                outcomes = ['finish_event', 'finish_test_phase'],
+                input_keys = ['cmd_input'])
+        # Publisher
+        self.pub_follow_req = rospy.Publisher('/chase/request', String, queue_size = 1)
+        # Survice
+        self.ggi_learning_srv = rospy.ServiceProxy('/ggi_learning', GgiLearning)
+        self.test_phase_srv = rospy.ServiceProxy('/ggi_test_phase', GgiTestPhase)
+        self.yesno_srv = rospy.ServiceProxy('/yes_no', YesNo)
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state: EVENT')
+        if userdata.cmd_input == 'follow me':
+            speak('I will follow you')
+            self.pub_follow_req.publish('start')
+        elif userdata.cmd_input == 'stop following':
+            speak('Stop following')
+            self.pub_follow_req.publish('stop')
+        elif userdata.cmd_input == 'start ggi training':
+            speak('Start ggi learning')
+            #self.ggi_learning_srv()
+        elif userdata.cmd_input == 'start test phase':
+            speak('Can i start the test phase?')
+            result = self.yesno_srv()
+            if result.result:
+                speak('Start the test phase')
+                result = self.test_phase_srv().result
+                print result
+                speak('Finish test phase')
+                return 'finish_test_phase'
+            else:
+                speak('OK, Continue the training phase')
+        else:
+            pass
+        return 'finish_event'
+
+
+def main():
+    sm_top = smach.StateMachine(outcomes = ['finish_sm_top'])
+
+    with sm_top:
+        smach.StateMachine.add(
+                'LISTEN',
+                Listen(),
+                transitions = {'listen_failed':'LISTEN',
+                               'motion':'MOTION',
+                               'event':'EVENT'},
+                remapping = {'cmd_output':'cmd_name'})
+
+        smach.StateMachine.add(
+                'MOTION',
+                Motion(),
+                transitions = {'finish_motion':'LISTEN'},
+                remapping = {'cmd_input':'cmd_name'})
+
+        smach.StateMachine.add(
+                'EVENT',
+                Event(),
+                transitions = {'finish_event':'LISTEN',
+                               'finish_test_phase':'finish_sm_top'},
+                remapping = {'cmd_input':'cmd_name'})
+
+    outcome = sm_top.execute()
+
+
+if __name__ == '__main__':
+    rospy.init_node('sm_training_phsase', anonymous = True)
+    main()
